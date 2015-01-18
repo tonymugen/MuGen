9ab252fcddc22661e12e2fc09beb6c6e4e42d8ce
Anthony J Greenberg <tonygreen2655@gmail.com>
2015-01-18 00:23:15 -0500

    added GPL

--- MuGenLib/makeROC.cpp
+++ MuGenLib/makeROC.cpp
@@ -1,11 +1,29 @@
 /*
-*  makeROC.cpp
-*  MuGenLib
-*
-*  Created by ajg67 on 2/6/13.
-*   Copyright (c) 2013 SEELE. All rights reserved.
-
-	Making ROC curves from simulated data analyses
+ *  makeROC.cpp
+ *  MuGenLib
+ *
+ *
+ *	Making ROC curves from simulated data analyses
+ *
+ *  Copyright (c) 2015 Anthony J. Greenberg
+ *
+ *  This file is part of the MuGen library.
+ *
+ *  MuGen is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  MuGen is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with MuGen.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  A set of functions to read and save GSL matrices in binary from R
+ *  to compile: R CMD SHLIB GSLRIO.cpp -lgsl -O3 -DHAVE_INLINE -DGSL_RANGE_CHECK_OFF
 */
 
 #include <omp.h>
